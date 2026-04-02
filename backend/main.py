e0d13a310ecc5318d4e3479582361b4ae6a2ade9
Utkarsh Kumar <126360830+Uk8852@users.noreply.github.com>
2026-04-02 18:16:24 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -5,11 +5,9 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-app = FastAPI()
-
 origins = [
     "http://localhost:5173",
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
     "https://task-manager-yn0x.onrender.com"
 ]
 
