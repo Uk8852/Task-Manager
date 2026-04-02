655cf8893026289cae7a5d42220b396a1ced4dbe
Utkarsh Kumar <126360830+Uk8852@users.noreply.github.com>
2026-04-02 17:42:15 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import API from "./api";
 import TaskForm from "./components/TaskForm";
 import TaskList from "./components/TaskList";
-import "./styles.css";
+import "./app.css";
 
 function App() {
   const [tasks, setTasks] = useState([]);
